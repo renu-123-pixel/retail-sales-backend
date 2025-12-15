df9504f3a62e2f06fe9ff25a6266e5e4533923ee
Renu Ekka <114815478+renu-123-pixel@users.noreply.github.com>
2025-12-15 17:28:01 +0530

    Add allowed origin for production environment

--- src/server.js
+++ src/server.js
@@ -18,6 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
  const allowedOrigins = [
+  https://retail-sales-management-system-blue.vercel.app,
   "http://localhost:5173",
   "http://localhost:3000",
   "http://127.0.0.1:5173"
